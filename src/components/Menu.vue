59e72514c0546854ddb4f891ca65649b2bb2cf61
ABSOLUTE\MHNguyen <MHNguyen@absolute.com>
2018-07-22 20:13:09 +0700

    Submenu display

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -4,9 +4,14 @@
         <nav role="navigation">
             <ul class="access-menu">
                 <li>
-                    <a href="#" v-on:mouseover="displayMenu('womens')" v-on:mouseout="hideMenu('womens')">Womens</a>
+                    <a href="#"
+                        v-on:mouseover="displayMenu('womens')"
+                        v-on:mouseout="hideMenu('womens')"
+                        v-bind:class="{ 'display-menu': displayControls.womens.isDisplay }">Womens</a>
                     <ul class="access-submenu"
-                        v-bind:class="{ 'display-menu': displayControls.womens }">
+                        v-on:mouseover="displayMenu('womens')"
+                        v-on:mouseout="hideMenu('womens')"
+                        v-bind:class="{ 'display-menu': displayControls.womens.isDisplay }">
                         <li><a href="#">Featured</a></li>
                         <li><a href="#">Shoes</a></li>
                         <li><a href="#">Clothing</a></li>
@@ -16,9 +21,14 @@
                     </ul>
                 </li>
                 <li>
-                    <a href="#" v-on:mouseover="displayMenu('mens')" v-on:mouseout="hideMenu('mens')">Mens</a>
+                    <a href="#"
+                        v-on:mouseover="displayMenu('mens')"
+                        v-on:mouseout="hideMenu('mens')"
+                        v-bind:class="{ 'display-menu': displayControls.mens.isDisplay }">Mens</a>
                     <ul class="access-submenu"
-                        v-bind:class="{ 'display-menu': displayControls.mens }">
+                        v-on:mouseover="displayMenu('mens')"
+                        v-on:mouseout="hideMenu('mens')"
+                        v-bind:class="{ 'display-menu': displayControls.mens.isDisplay }">
                         <li><a href="#">Featured</a></li>
                         <li><a href="#">Shoes</a></li>
                         <li><a href="#">Clothing</a></li>
@@ -28,9 +38,14 @@
                     </ul>
                 </li>
                 <li>
-                    <a href="#" v-on:mouseover="displayMenu('kids')" v-on:mouseout="hideMenu('kids')">Kids</a>
+                    <a href="#"
+                        v-on:mouseover="displayMenu('kids')"
+                        v-on:mouseout="hideMenu('kids')"
+                        v-bind:class="{ 'display-menu': displayControls.kids.isDisplay }">Kids</a>
                     <ul class="access-submenu"
-                        v-bind:class="{ 'display-menu': displayControls.kids }">
+                        v-on:mouseover="displayMenu('kids')"
+                        v-on:mouseout="hideMenu('kids')"
+                        v-bind:class="{ 'display-menu': displayControls.kids.isDisplay }">
                         <li><a href="#">Featured</a></li>
                         <li><a href="#">Boys</a></li>
                         <li><a href="#">Girls</a></li>
@@ -39,9 +54,14 @@
                     </ul>
                 </li>
                 <li>
-                    <a href="#" v-on:mouseover="displayMenu('sale')" v-on:mouseout="hideMenu('sale')">Sale</a>
+                    <a href="#"
+                        v-on:mouseover="displayMenu('sale')"
+                        v-on:mouseout="hideMenu('sale')"
+                        v-bind:class="{ 'display-menu': displayControls.sale.isDisplay }">Sale</a>
                     <ul class="access-submenu"
-                        v-bind:class="{ 'display-menu': displayControls.sale }">
+                        v-on:mouseover="displayMenu('sale')"
+                        v-on:mouseout="hideMenu('sale')"
+                        v-bind:class="{ 'display-menu': displayControls.sale.isDisplay }">
                         <li><a href="#">Men's Sale</a></li>
                         <li><a href="#">Women's Sale</a></li>
                         <li><a href="#">Plus Sale</a></li>
@@ -58,35 +78,56 @@
     data () {
         return {
             displayControls: {
-                womens: false,
-                mens: false,
-                kids: false,
-                sale: false
+                womens: {
+                    isDisplay: false,
+                    sucMenus: {
+                        
+                    }
+                },
+                mens: {
+                    isDisplay: false
+                },
+                kids: {
+                    isDisplay: false
+                },
+                sale: {
+                    isDisplay: false
+                }
             }
         }
     },
     methods: {
         displayMenu(sectionName) {
             let section = '';
-            this.displayControls[sectionName] = true;
             for (section in this.displayControls) {
                 if (section === sectionName) {
-                    this.displayControls[section] = true;
+                    this.displayControls[section].isDisplay = true;
                 } else {
-                    this.displayControls[section] = false;
+                    this.displayControls[section].isDisplay = false;
                 }
             }
         },
         hideMenu (sectionName) {
-            this.displayControls[sectionName] = false;
+            this.displayControls[sectionName].isDisplay = false;
         }
     },
     mounted() {
         this.displayControls = {
-                womens: false,
-                mens: false,
-                kids: false,
-                sale: false
+                womens: {
+                    isDisplay: false,
+                    sucMenus: {
+                        
+                    }
+                },
+                mens: {
+                    isDisplay: false
+                },
+                kids: {
+                    isDisplay: false
+                },
+                sale: {
+                    isDisplay: false
+                }
             };
     }
 }
@@ -144,6 +185,10 @@
             -ms-transition: all .2s linear;
             -o-transition: all .2s linear;
             transition: all .2s linear;
+        }
+
+        .access-menu a.display-menu {
+            background-color: cadetblue;
         }
 
         .access-submenu {
